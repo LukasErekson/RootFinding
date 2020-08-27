9ff01f40a908e28fb33dbc0564a14366e8abeb22
Lukas Erekson <Lukas.Erekson@gmail.com>
2020-08-26 23:17:01 -0600

    Fix formatting/style in RootTracker.py

--- yroots/RootTracker.py
+++ yroots/RootTracker.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def rootInBox(root, a, b):
     """Checks to see if a root is in a box.
 
@@ -18,16 +19,19 @@ def rootInBox(root, a, b):
     """
     return np.all(root > a) and np.all(root < b)
 
+
 class RootTracker:
     '''
-    Class to track the roots that are found found using the subdivision solver.
+    Class to track the roots that are found found using the subdivision
+    solver.
 
     Attributes
     ----------
     roots: numpy array
         The roots of the system being solved
     possible_duplicates : list
-        Roots that were outside their search interval so might be duplicates
+        Roots that were outside their search interval, so they might be
+        duplicates.
     potential_roots : numpy array
         Places that may or may not have a root that we found.
     intervals : list
@@ -42,11 +46,12 @@ class RootTracker:
     __init__
         Initializes everything.
     add_roots
-        Adds roots the were found to the list, along with their information.
+        Adds roots the were found to the list, along with their
+        information.
     add_potential_roots
-        Adds roots that were found by the solver but are questionable. We will
-        want to double check that these roots aren't duplicated elsewhere or that
-        they give a fairly good answer.
+        Adds roots that were found by the solver but are questionable.
+        We will want to double check that these roots aren't duplicated
+        elsewhere or that they give a fairly good answer.
     get_polish_intervals
         Gets the intervals to run the next round of polishing on.
     '''
@@ -56,12 +61,13 @@ class RootTracker:
         self.potential_roots = np.array([])
         self.intervals = []
         self.methods = []
-        #for tracking condition numbers and gradients
+        # for tracking condition numbers and gradients
         self.conds = []
         self.grads = []
 
     def add_roots(self, zeros, a, b, method):
-        ''' Store the roots that were found, along with the interval they were found in and the method used.
+        ''' Store the roots that were found, along with the interval
+        they were found in and the method used.
 
         Parameters
         ----------
@@ -79,7 +85,7 @@ class RootTracker:
                 self.add_root(zero, a, b, method)
             else:
                 found = False
-                for a_,b_ in self.intervals:
+                for a_, b_ in self.intervals:
                     if rootInBox(zero, a_, b_):
                         found = True
                         break
@@ -94,26 +100,9 @@ class RootTracker:
                 temp.append([zero, a_, b_, method])
         self.possible_duplicates = temp
 
-
-#         if not isinstance(a, np.ndarray):
-#             dim = 1
-#         else:
-#             dim = len(a)
-#         if len(self.roots) == 0:
-#             if dim == 1:
-#                 self.roots = np.zeros([0])
-#             else:
-#                 self.roots = np.zeros([0,dim])
-
-#         if dim > 1:
-#             self.roots = np.vstack([self.roots, zeros])
-#         else:
-#             self.roots = np.hstack([self.roots, zeros])
-#         self.intervals += [(a,b)]*len(zeros)
-#         self.methods += [method]*len(zeros)
-
     def add_root(self, zero, a, b, method):
-        ''' Store the root that was found, along with the interval it was found in and the method used.
+        ''' Store the root that was found, along with the interval it
+        was found in and the method used.
 
         Parameters
         ----------
@@ -134,17 +123,17 @@ class RootTracker:
             if dim == 1:
                 self.roots = np.zeros([0])
             else:
-                self.roots = np.zeros([0,dim])
+                self.roots = np.zeros([0, dim])
         if dim > 1:
             self.roots = np.vstack([self.roots, zero])
         else:
             self.roots = np.hstack([self.roots, zero])
-        self.intervals += [(a,b)]
+        self.intervals += [(a, b)]
         self.methods += [method]
 
     def add_potential_roots(self, potentials, a, b, method):
-        ''' Store the potential roots that were found, along with the interval
-        they were found in and the method used.
+        ''' Store the potential roots that were found, along with the
+        interval they were found in and the method used.
 
         Parameters
         ----------
@@ -165,26 +154,27 @@ class RootTracker:
             if dim == 1:
                 self.potential_roots = np.zeros([0])
             else:
-                self.potential_roots = np.zeros([0,dim])
+                self.potential_roots = np.zeros([0, dim])
 
         if dim > 1:
             self.potential_roots = np.vstack([self.potential_roots, potentials])
         else:
             self.potential_roots = np.hstack([self.potential_roots, potentials])
-        self.intervals += [(a,b)]*len(potentials)
+        self.intervals += [(a, b)]*len(potentials)
         self.methods += [method]*len(potentials)
 
     def get_polish_intervals(self):
         ''' Find the intervals to run the polishing on.
 
-        Deletes the rest of the info as subdivision will be rerun on these intervals.
+        Deletes the rest of the info as subdivision will be rerun on
+        these intervals.
 
         returns
         -------
         polish_intervals : list
             The intervals to rerun the search on.
         '''
-        polish_intervals = np.unique(self.intervals,axis=0)
+        polish_intervals = np.unique(self.intervals, axis=0)
         self.intervals = []
         self.roots = []
         self.methods = []
